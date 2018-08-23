ad9eca0c53664f0b523447784199e1d8b724ff50
z-lamb <lamb.zachary@gmail.com>
2018-08-22 23:40:27 -0600

    finish jthw ex 1

---
+++ JavaTheHardWay/FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+	public static void main( String[] args ) {
+		System.out.println( "Zach is determined to learn how to code." );
+		System.out.println( "Today's date is Sunday, August 19, 2018" );
+	}
+}
