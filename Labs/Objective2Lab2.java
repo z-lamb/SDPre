2a5db877887c7426d225adbb711e2cdd12e71c8d
z-lamb <lamb.zachary@gmail.com>
2018-08-24 10:52:41 -0600

    complete Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,11 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Zachary Lamb";
+    int age = 30;
+    String state = "California";
+
+    System.out.print("Name: " + name + " ");
+    System.out.print("Age: " + age + " ");
+    System.out.println("State: " + state);
+  }
+}
