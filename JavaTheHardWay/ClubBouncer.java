fc961120b90c14f7f49ac3ac63e37ab9dccbc659
z-lamb <lamb.zachary@gmail.com>
2018-08-24 15:57:49 -0600

    finish jthw ex 17

---
+++ JavaTheHardWay/ClubBouncer.java
@@ -0,0 +1,16 @@
+public class ClubBouncer {
+	public static void main( String[] args ) {
+		int age = 22;
+		boolean onGuestList = false;
+		double allure = 7.5;
+		String gender = "F";
+
+		if ( onGuestList || age >= 21 || (gender.equals("F") && allure >= 8) ) {
+			System.out.println("You are allowed to enter the club.");
+		}
+		// System.out.println( "C-C-C-COMBO BREAKER" );
+		else {
+			System.out.println("You are not allowed to enter the club.");
+		}
+	}
+}
