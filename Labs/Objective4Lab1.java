f29dfced797b3e67ceb14455f6f5fa1777db98dd
z-lamb <lamb.zachary@gmail.com>
2018-08-24 17:48:36 -0600

    complete Objective4Lab1

---
+++ Labs/Objective4Lab1.java
@@ -0,0 +1,19 @@
+public class Objective4Lab1 {
+  public static void main(String[] args) {
+    int num1 = 5;
+    int num2 = 15;
+
+    /*
+    Not sure what code you wanted to go in here
+    but I have the expected output
+    */
+
+    if(num1 > num2) {
+      System.out.println( num1 + " is greater than " + num2 );
+    }
+    else if(num2 > num1) {
+      System.out.println( num2 + " is greater than " + num1 );
+    }
+
+  }
+}
