9ab48ed61aba4c55811b862b1edd900500a228d6
z-lamb <lamb.zachary@gmail.com>
2018-08-23 20:47:21 -0600

    completed Objective1Lab3

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,7 @@
+public class Objective1Lab3 {
+  public static void main(String[] args) {
+    System.out.println("Name : Zachary Lamb");
+    System.out.println("Age : 30");
+    System.out.println("State : California");
+  }
+}
