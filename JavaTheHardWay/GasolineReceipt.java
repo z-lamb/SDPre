97f821494565da5ec9cbe4175a77616d7e8455b6
z-lamb <lamb.zachary@gmail.com>
2018-08-23 19:44:33 -0600

    finish jthw ex 2

---
+++ JavaTheHardWay/GasolineReceipt.java
@@ -0,0 +1,16 @@
+public class GasolineReceipt {
+	public static void main( String[] args ) {
+		System.out.println( "+------------------------+" );
+		System.out.println( "|                        |" );
+		System.out.println( "|      CORNER STORE      |" );
+		System.out.println( "|                        |" );
+		System.out.println( "| 2018-10-19  03:25PM    |" );
+		System.out.println( "|                        |" );
+		System.out.println( "| Gallons:       10.870  |" );
+		System.out.println( "| Price/gallon: $ 2.089  |" );
+		System.out.println( "|                        |" );
+		System.out.println( "| Fuel total:  $ 22.71   |" );
+		System.out.println( "|                        |" );
+		System.out.println( "+------------------------+" );
+	}
+}
