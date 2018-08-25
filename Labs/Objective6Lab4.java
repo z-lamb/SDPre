87a53f840796b962620fde2bcc5d2bbdd7246054
z-lamb <lamb.zachary@gmail.com>
2018-08-24 23:53:17 -0600

    complete Objective6Lab4

---
+++ Labs/Objective6Lab4.java
@@ -0,0 +1,11 @@
+public class Objective6Lab4 {
+  public static void main(String[] args) {
+    int count = 1, sum = 0;
+
+    while(count <=20) {
+      sum = sum + count;
+      count++;
+    }
+    System.out.println(sum);
+  }
+}
