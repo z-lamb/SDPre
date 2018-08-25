c62140f27f70b5d2ac3508a2e91e0a26b934bfb9
z-lamb <lamb.zachary@gmail.com>
2018-08-24 21:34:47 -0600

    finish jthw ex 25

---
+++ JavaTheHardWay/EnterPIN.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class EnterPIN {
+	public static void main( String[] args ) {
+		Scanner keyboard = new Scanner(System.in);
+		int pin, entry;
+		String password, userPwd;
+
+		System.out.println("WELCOME TO THE BANK OF JAVA.");
+    System.out.print("ENTER YOUR PASSWORD: ");
+
+    password = "help";
+    userPwd = keyboard.next();
+
+		while ( ! userPwd.equals(password) ) {
+			System.out.println("\nINCORRECT PASSWORD. TRY AGAIN.");
+			System.out.print("ENTER YOUR PASSWORD: ");
+			userPwd = keyboard.next();
+		}
+
+		pin = 12345;
+
+    System.out.print("ENTER YOUR PIN: ");
+		entry = keyboard.nextInt();
+
+		while (entry != pin ) {
+			System.out.println("\nINCORRECT PIN. TRY AGAIN.");
+			System.out.print("ENTER YOUR PIN: ");
+			entry = keyboard.nextInt();
+		}
+
+		System.out.println("\nPIN ACCEPTED. YOUR ACCOUNT BALANCE IS $425.17");
+	}
+}
